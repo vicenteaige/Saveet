e5c39971e9aa74bc79330e7bc2f533b428238b85
Xavier Velàsquez <xvelasquez@lsi.upc.edu>
2015-07-14 18:20:06 +0200

    Sample user seeder added. API login call working. API calls now bypass CSRF token verification.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -29,9 +29,7 @@ Route::group(['prefix' => 'v1'], function () {
         Route::get('login', function() {
             //
         });
-        Route::post('login', function() {
-            //
-        });
+        Route::post('login', 'UserController@apiLogUser');
         Route::post('register', function() {
            //
         });
