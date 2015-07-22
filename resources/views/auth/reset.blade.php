ec872ce0350798e6e4162e2be5cb8faf4b52c7ac
Vicente Aige <vicenteaige@gmail.com>
2015-07-22 12:18:04 +0200

    Logo + password reset with bootstrap

--- resources/views/auth/reset.blade.php
+++ resources/views/auth/reset.blade.php
@@ -1,37 +1,42 @@
 <!DOCTYPE html>
 <html ng-app="reset">
 <head>
-    <title>Register</title>
+    <title>Password reset</title>
     <script src="/bower_components/angular/angular.js"></script>
     <script src="/bower_components/angular-resource/angular-resource.js"></script>
     <script src="/bower_components/jquery/dist/jquery.js"></script>
     <script src="/bower_components/bootstrap/js/bootstrap.js"></script>
     <link rel="stylesheet" href="/bower_components/bootstrap/css/bootstrap.css" type="text/css">
+    <link rel="stylesheet" href="/css/style.css">
     <script src="/js/reset.js"></script>
 </head>
 <body>
 
+
 <div class="container" ng-controller="MainController">
-<!-- <form method="POST" action="/password/reset"> -->
 
-    <input type="hidden" name="token" value="{{ $token }}">
-    <div>
-        Password
-        <input type="password" ng-model="sendPassword" name="password">
+    <div class="form-signin">
+
+        <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
+
+        <h2 class="form-signin-heading">Password reset</h2>
+        <p>Type your new password</p>
+
+        <label for="inputPassword" class="sr-only"></label>
+        <input type="password" ng-model="sendPassword" name="password" class="form-control no_radius_bottom bottom_less1" placeholder="Password" required autofocus>
+
+        <label for="inputPassword" class="sr-only"></label>
+        <input type="password" ng-model="sendPasswordConfirmation" name="password_confirmation" class="form-control no_radius_top bottom_plus10" placeholder="Password confirmation" >
+
+        <button class="btn btn-lg btn-primary btn-block"  ng-click="reset(sendPassword, sendPasswordConfirmation)">Reset password</button>
+        <div id="signup">
+            <label><a href="login">Do you have an account? Sign In</a></label>
+        </div>
+
+
     </div>
 
-    <div>
-        Confirm Password
-        <input type="password" ng-model="sendPasswordConfirmation" name="password_confirmation">
-    </div>
-
-    <div>
-        <button ng-click="reset(sendPassword, sendPasswordConfirmation)">
-            Reset Password
-        </button>
-    </div>
-<!-- </form> -->
-</div>
+</div> <!-- /container -->
 
 </body>
 
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -71,13 +71,15 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">Bootcamp 1</a>
+                <a class="navbar-brand" href="#">
+                    <img alt="Brand" src="img/saveet.png">
+                </a>
             </div>
             <div id="navbar" class="collapse navbar-collapse">
                 <ul class="nav navbar-nav">
                     <li class="active"><a href="#">Home</a></li>
-                    <li><a href="#about">Daily</a></li>
-                    <li><a href="#contact">Cities</a></li>
+                    <li><a href="#">Daily</a></li>
+                    <li><a href="#">Cities</a></li>
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
                     <li class="dropdown">
@@ -101,6 +103,7 @@
             <h1>Welcome, {{ Auth::user()->name }}</h1>
             <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
         </div>
+
 
     </div><!-- /.container -->
     </body>
