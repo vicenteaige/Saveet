c29e9f98c0a337a12ec93614d221a845314a3d7d
Vicente Aige <vicenteaige@gmail.com>
2015-07-23 17:57:23 +0200

    index css: navbar/header opacity and position.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -23,12 +23,13 @@
         <script type="application/javascript" src="/js/logout.js"></script>
         <script type="application/javascript" src="/js/tag.js"></script>
         <link rel="stylesheet" href="/css/index.css">
+        <link rel="stylesheet" href="/css/style.css">
 
         <!--<script src="http://d3js.org/d3.v3.min.js" charset="utf-8"></script>-->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js"></script>
         
         <script>var rootApp = angular.module('rootApp', ['logout','usertags'])</script>
-        
+
         <style>
 
             .plot {
@@ -108,117 +109,120 @@
 
             </div>
         </div>
+        <div class="graphic">
+            <div class="plot" style="text-align: center">
+                <script>
+                    var margin = {top: 20, right: 80, bottom: 30, left: 50},
+                            width = 960 - margin.left - margin.right,
+                            height = 500 - margin.top - margin.bottom;
+
+                    var parseDate = d3.time.format("%Y%m%d").parse;
+
+                    var x = d3.time.scale()
+                            .range([0, width]);
+
+                    var y = d3.scale.linear()
+                            .range([height, 0]);
+
+                    var color = d3.scale.category10();
+
+                    var xAxis = d3.svg.axis()
+                            .scale(x)
+                            .orient("bottom");
+
+                    var yAxis = d3.svg.axis()
+                            .scale(y)
+                            .orient("left");
+
+                    var line = d3.svg.line()
+                            .interpolate("basis")
+                            .x(function(d) { return x(d.date); })
+                            .y(function(d) { return y(d.tweets); });
+
+                    var svg = d3.select(".plot").append("svg")
+                            .attr("width", width + margin.left + margin.right)
+                            .attr("height", height + margin.top + margin.bottom)
+                            .append("g")
+                            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+                    d3.json("/test/json", function(error, data) {
+                        if (error) throw error;
+                        color.domain(d3.keys(data[0]).filter(function(key) { return key !== "date"; }));
+                        data.forEach(function(d) {
+                            d.date = parseDate(String(d.date));
+                        });
+
+                        var cities = color.domain().map(function(name) {
+                            return {
+                                name: name,
+                                values: data.map(function(d) {
+                                    return {date: d.date, tweets: +d[name]};
+                                })
+                            };
+                        });
+
+                        x.domain(d3.extent(data, function(d) { return d.date; }));
+
+                        y.domain([
+                            d3.min(cities, function(c) { return d3.min(c.values, function(v) { return v.tweets; }); }),
+                            d3.max(cities, function(c) { return d3.max(c.values, function(v) { return v.tweets; }); })
+                        ]);
+
+                        svg.append("g")
+                                .attr("class", "x axis")
+                                .attr("transform", "translate(0," + height + ")")
+                                .call(xAxis);
+
+                        svg.append("g")
+                                .attr("class", "y axis")
+                                .call(yAxis)
+                                .append("text")
+                                .attr("transform", "rotate(-90)")
+                                .attr("y", 6)
+                                .attr("dy", ".71em")
+                                .style("text-anchor", "end")
+                                .text("Tweets");
+
+                        var tag = svg.selectAll(".tag")
+                                .data(cities)
+                                .enter().append("g")
+                                .attr("class", "tag");
+
+                        tag.append("path")
+                                .attr("class", "line")
+                                .attr("d", function(d) { return line(d.values); })
+                                .style("stroke", function(d) { return color(d.name); });
+
+                        tag.append("text")
+                                .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
+                                .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.tweets) + ")"; })
+                                .attr("x", 3)
+                                .attr("dy", ".35em")
+                                .text(function(d) { return d.name; });
+
+                        var path = svg.append("path")
+                                .attr("d", line(data))
+                                .attr("stroke", "steelblue")
+                                .attr("stroke-width", "2")
+                                .attr("fill", "none");
+
+                        var totalLength = path.node().getTotalLength();
+
+                        path
+                                .attr("stroke-dasharray", totalLength + " " + totalLength)
+                                .attr("stroke-dashoffset", totalLength)
+                                .transition()
+                                .duration(2000)
+                                .ease("linear")
+                                .attr("stroke-dashoffset", 0);
+                    });
+
+                </script>
+            </div>
+        </div>
     </div><!-- /.container -->
 
-    <div class="plot" style="text-align: center">
-        <script>
-            var margin = {top: 20, right: 80, bottom: 30, left: 50},
-                    width = 960 - margin.left - margin.right,
-                    height = 500 - margin.top - margin.bottom;
 
-            var parseDate = d3.time.format("%Y%m%d").parse;
-
-            var x = d3.time.scale()
-                    .range([0, width]);
-
-            var y = d3.scale.linear()
-                    .range([height, 0]);
-
-            var color = d3.scale.category10();
-
-            var xAxis = d3.svg.axis()
-                    .scale(x)
-                    .orient("bottom");
-
-            var yAxis = d3.svg.axis()
-                    .scale(y)
-                    .orient("left");
-
-            var line = d3.svg.line()
-                    .interpolate("basis")
-                    .x(function(d) { return x(d.date); })
-                    .y(function(d) { return y(d.tweets); });
-
-            var svg = d3.select(".plot").append("svg")
-                    .attr("width", width + margin.left + margin.right)
-                    .attr("height", height + margin.top + margin.bottom)
-                    .append("g")
-                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-            d3.json("/test/json", function(error, data) {
-                if (error) throw error;
-                color.domain(d3.keys(data[0]).filter(function(key) { return key !== "date"; }));
-                data.forEach(function(d) {
-                    d.date = parseDate(String(d.date));
-                });
-
-                var cities = color.domain().map(function(name) {
-                    return {
-                        name: name,
-                        values: data.map(function(d) {
-                            return {date: d.date, tweets: +d[name]};
-                        })
-                    };
-                });
-
-                x.domain(d3.extent(data, function(d) { return d.date; }));
-
-                y.domain([
-                    d3.min(cities, function(c) { return d3.min(c.values, function(v) { return v.tweets; }); }),
-                    d3.max(cities, function(c) { return d3.max(c.values, function(v) { return v.tweets; }); })
-                ]);
-
-                svg.append("g")
-                        .attr("class", "x axis")
-                        .attr("transform", "translate(0," + height + ")")
-                        .call(xAxis);
-
-                svg.append("g")
-                        .attr("class", "y axis")
-                        .call(yAxis)
-                        .append("text")
-                        .attr("transform", "rotate(-90)")
-                        .attr("y", 6)
-                        .attr("dy", ".71em")
-                        .style("text-anchor", "end")
-                        .text("Tweets");
-
-                var tag = svg.selectAll(".tag")
-                        .data(cities)
-                        .enter().append("g")
-                        .attr("class", "tag");
-
-                tag.append("path")
-                        .attr("class", "line")
-                        .attr("d", function(d) { return line(d.values); })
-                        .style("stroke", function(d) { return color(d.name); });
-
-                tag.append("text")
-                        .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
-                        .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.tweets) + ")"; })
-                        .attr("x", 3)
-                        .attr("dy", ".35em")
-                        .text(function(d) { return d.name; });
-
-                var path = svg.append("path")
-                        .attr("d", line(data))
-                        .attr("stroke", "steelblue")
-                        .attr("stroke-width", "2")
-                        .attr("fill", "none");
-
-                var totalLength = path.node().getTotalLength();
-
-                path
-                        .attr("stroke-dasharray", totalLength + " " + totalLength)
-                        .attr("stroke-dashoffset", totalLength)
-                        .transition()
-                        .duration(2000)
-                        .ease("linear")
-                        .attr("stroke-dashoffset", 0);
-            });
-
-        </script>
-    </div>
     <footer class="footer">
         <div class="git-logo">
             <p class="text-muted">Check this project on GitHub
