edbc05e19967ad58d0deaf458d9591ccc078646e
jlightyear <joseluis.estrach@gmail.com>
2015-07-23 16:36:25 +0200

    User activation with authentication and redirect to home

--- app/Http/Controllers/ActivateController.php
+++ app/Http/Controllers/ActivateController.php
@@ -7,20 +7,21 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\User;
+use Auth;
+use Redirect;
 
 class ActivateController extends Controller
 {
     public function store($token)
     {
-        $user = User::where('activateToken', '=', $token);
+        if($token){
+            $user = User::where('activateToken', $token)->first();
 
-        if(!$user){
-            return response()->api(400, 'no', 'No hay usuario', '');
+            if($user){
+                $user->update(['active' => 1]);
+                Auth::login($user);
+            }
+            return Redirect('login');
         }
-
-        $user->update(['active' => 1]);
-
-        return response()->api(200, 'yes', '', '');
-
     }
 }
